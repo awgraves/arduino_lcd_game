6987e9d65f9aa8638cf7e9f4343885c0ec0a4bf6
Andrew Graves <andrewgraves92@gmail.com>
2026-04-30 16:28:23 -0400

    simplify jump and camera update logic

--- lib/game/game.cpp
+++ lib/game/game.cpp
@@ -24,12 +24,11 @@ void GameState_update(GameState *s, Inputs *in) {
 
 /* helpers */
 static bool is_blocked(GameState *s, int16_t x, int16_t y);
-static bool is_on_ground(GameState *s);
 
 static void update_player_pos(GameState *s, Inputs *in) {
   // gravity
   if (!s->player.on_ground && s->player.y_vel >= 0) {
-    // hang time
+    // hang time at the top
     if (s->player.y_vel == 0 && s->player.y_hold_remaining) {
       s->player.y_vel = 0;
       s->player.y_hold_remaining--;
@@ -39,26 +38,27 @@ static void update_player_pos(GameState *s, Inputs *in) {
   }
 
   // jump
-  if (s->player.on_ground && !s->sw_prev_pressed && in->sw_pressed) {
+  bool sw_just_pressed = in->sw_pressed && !s->sw_prev_pressed;
+  bool sw_just_released = !in->sw_pressed && s->sw_prev_pressed;
+  if (sw_just_pressed && s->player.on_ground) {
     // long jump (2 tiles)
     s->player.y_vel = 2;
     s->player.y_hold_remaining = 1;
-  } else if (!s->player.on_ground && s->sw_prev_pressed && !in->sw_pressed) {
-    // cut jump short (1 tile), user let press go early
+  } else if (sw_just_released && !s->player.on_ground) {
+    // cut jump short if user let go early
     s->player.y_vel = s->player.y_hold_remaining = 0;
   }
   s->sw_prev_pressed = in->sw_pressed;
 
+  // calc next potential axis destinations
   int16_t pot_y = s->player.y + (s->player.y_vel > 0 ? 1 : s->player.y_vel);
   int16_t pot_x = s->player.x + in->x_move;
 
   // resolve y move
   if (!is_blocked(s, s->player.x, pot_y)) {
     s->player.y = pot_y;
-  } else {
-    // if upward momentum blocked, reset it
-    if (s->player.y_vel > 0)
-      s->player.y_vel = 0;
+  } else if (s->player.y_vel > 0) {
+    s->player.y_vel = s->player.y_hold_remaining = 0;
   }
 
   // resolve x move
@@ -67,24 +67,15 @@ static void update_player_pos(GameState *s, Inputs *in) {
   }
 
   // check for ground
-  if ((s->player.on_ground = is_on_ground(s)))
+  if ((s->player.on_ground = is_blocked(s, s->player.x, s->player.y - 1)))
     s->player.y_vel = 0;
 }
 
 static void update_camera_pos(GameState *s) {
-  s->camera_x = (s->player.x - 8 >= 0) ? s->player.x - 8 : 0;
-
-  if (s->player.y - s->camera_y > 1) {
-    s->camera_y = s->player.y;
-  } else if (s->player.y - s->camera_y < 1 && s->player.y > 0) {
-    s->camera_y = s->player.y - 1;
-  }
+  s->camera_x = s->player.x - 8 >= 0 ? s->player.x - 8 : 0;
+  s->camera_y = s->player.y > 0 ? s->player.y - 1 : s->player.y;
 }
 
 static bool is_blocked(GameState *s, int16_t x, int16_t y) {
   return (y < 0) || s->map[x][y] == TILE_BLOCK;
-}
-
-static bool is_on_ground(GameState *s) {
-  return is_blocked(s, s->player.x, s->player.y - 1);
 }
--- lib/game/game.h
+++ lib/game/game.h
@@ -6,9 +6,10 @@ const int MAX_OBJECTS = 100;
 
 typedef enum { TILE_EMPTY, TILE_BLOCK, TILE_SPIKE, TILE_FLAG } Tile;
 
-const int MAX_MAP_HEIGHT = 10;
 const int MAX_MAP_WIDTH = 64;
+const int MAX_MAP_HEIGHT = 10;
 
+// note the width comes before the height to match x, y coord convention
 typedef Tile TileMap[MAX_MAP_WIDTH][MAX_MAP_HEIGHT];
 
 typedef enum { LEFT, RIGHT, CENTER } Direction;
@@ -19,8 +20,8 @@ typedef struct {
   struct {
     int16_t x;
     int16_t y;
-    int8_t y_vel;            // velocity -1, 0, 1, 2
-    int8_t y_hold_remaining; // hangtime in jump
+    int8_t y_vel;
+    int8_t y_hold_remaining;
     bool on_ground;
     Direction facing;
   } player;
